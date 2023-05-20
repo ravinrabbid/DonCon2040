d1de91862937fdd209258453a488237a7518ecdf
Frederik Walk <ravinrabbid@users.noreply.github.com>
2023-05-20 20:15:01 +0200

    Implement library for MCP23017 IO expander

---
+++ libs/mcp23017/include/mcp23017/Mcp23017.h
@@ -0,0 +1,71 @@
+#ifndef _MCP23017_MCP23017_H_
+#define _MCP23017_MCP23017_H_
+
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+
+class Mcp23017 {
+  public:
+    // Registers in IOCON.BANK = 0 mode
+    enum class Register {
+        IODIRA = 0x00,
+        IODIRB = 0x01,
+        IPOLA = 0x02,
+        IPOLB = 0x03,
+        GPINTENA = 0x04,
+        GPINTENB = 0x05,
+        DEFVALA = 0x06,
+        DEFVALB = 0x07,
+        INTCONA = 0x08,
+        INTCONB = 0x09,
+        IOCON = 0x0A,
+        GPPUA = 0x0C,
+        GPPUB = 0x0D,
+        INTFA = 0x0E,
+        INTFB = 0x0F,
+        INTCAPA = 0x10,
+        INTCAPB = 0x11,
+        GPIOA = 0x12,
+        GPIOB = 0x13,
+        OLATA = 0x14,
+        OLATB = 0x15,
+    };
+
+    enum class Port { A, B };
+    enum class Direction { IN, OUT };
+
+  private:
+    i2c_inst *m_i2c;
+    uint8_t m_address;
+
+  public:
+    Mcp23017(uint8_t address, i2c_inst *i2c);
+
+    void setDirection(uint16_t input_mask);
+    void setDirection(Port port, uint8_t input_mask);
+    void setDirection(uint8_t pin, Direction direction);
+    void setDirection(uint8_t pin, Port port, Direction direction);
+
+    void setPullup(uint16_t enable_mask);
+    void setPullup(Port port, uint8_t enable_mask);
+    void setPullup(uint8_t pin, bool enable);
+    void setPullup(uint8_t pin, Port port, bool enable);
+
+    uint16_t read();
+    uint8_t read(Port port);
+    bool read(uint8_t pin);
+    bool read(uint8_t pin, Port port);
+
+    void write(uint16_t value);
+    void write(Port port, uint8_t value);
+    void write(uint8_t pin, bool value);
+    void write(uint8_t pin, Port port, bool value);
+
+  private:
+    uint8_t readRegister8(Register reg);
+    uint16_t readRegister16(Register reg);
+    void writeRegister8(Register reg, uint8_t value);
+    void writeRegister16(Register reg, uint16_t value);
+};
+
+#endif // _MCP23017_MCP23017_H_
---
+++ libs/mcp23017/src/Mcp23017.cpp
@@ -0,0 +1,218 @@
+#include "Mcp23017.h"
+
+Mcp23017::Mcp23017(uint8_t address, i2c_inst *i2c) : m_i2c(i2c), m_address(address) {
+
+    // Sequential register addresses with automatic address pointer increment
+    writeRegister8(Register::IOCON, 0x00);
+}
+
+void Mcp23017::setDirection(uint16_t input_mask) { writeRegister16(Register::IODIRA, input_mask); }
+
+void Mcp23017::setDirection(Mcp23017::Port port, uint8_t input_mask) {
+    switch (port) {
+    case Port::A:
+        return writeRegister8(Register::IODIRA, input_mask);
+    case Port::B:
+        return writeRegister8(Register::IODIRB, input_mask);
+    }
+}
+void Mcp23017::setDirection(uint8_t pin, Mcp23017::Direction direction) {
+    if (pin > 15) {
+        return;
+    }
+
+    if (pin > 7) {
+        return setDirection(pin - 8, Port::B, direction);
+    }
+    return setDirection(pin, Port::A, direction);
+}
+
+void Mcp23017::setDirection(uint8_t pin, Mcp23017::Port port, Mcp23017::Direction direction) {
+    if (pin > 7) {
+        return;
+    }
+
+    Register target_register = Register::IODIRA;
+
+    switch (port) {
+    case Port::A:
+        target_register = Register::IODIRA;
+        break;
+    case Port::B:
+        target_register = Register::IODIRB;
+        break;
+    }
+
+    uint8_t iodirs = readRegister8(target_register);
+
+    switch (direction) {
+    case Direction::IN:
+        return writeRegister8(target_register, iodirs | (1 << pin));
+    case Direction::OUT:
+        return writeRegister8(target_register, iodirs & ~(1 << pin));
+    }
+}
+
+void Mcp23017::setPullup(uint16_t enable_mask) { writeRegister16(Register::GPPUA, enable_mask); }
+
+void Mcp23017::setPullup(Mcp23017::Port port, uint8_t enable_mask) {
+    switch (port) {
+    case Port::A:
+        return writeRegister8(Register::GPPUA, enable_mask);
+    case Port::B:
+        return writeRegister8(Register::GPPUB, enable_mask);
+    }
+}
+
+void Mcp23017::setPullup(uint8_t pin, bool enable) {
+    if (pin > 15) {
+        return;
+    }
+
+    if (pin > 7) {
+        return setPullup(pin - 8, Port::B, enable);
+    }
+    return setPullup(pin, Port::A, enable);
+}
+
+void Mcp23017::setPullup(uint8_t pin, Mcp23017::Port port, bool enable) {
+    if (pin > 7) {
+        return;
+    }
+
+    Register target_register = Register::GPPUA;
+
+    switch (port) {
+    case Port::A:
+        target_register = Register::GPPUA;
+        break;
+    case Port::B:
+        target_register = Register::GPPUB;
+        break;
+    }
+
+    uint8_t pullups = readRegister8(target_register);
+
+    if (enable) {
+        return writeRegister8(target_register, pullups | (1 << pin));
+    } else {
+        return writeRegister8(target_register, pullups & ~(1 << pin));
+    }
+}
+
+uint16_t Mcp23017::read() { return readRegister16(Register::GPIOA); }
+
+uint8_t Mcp23017::read(Port port) {
+    switch (port) {
+    case Port::A:
+        return readRegister8(Register::GPIOA);
+    case Port::B:
+        return readRegister8(Register::GPIOB);
+    }
+    return 0;
+}
+
+bool Mcp23017::read(uint8_t pin) {
+    if (pin > 15) {
+        return false;
+    }
+
+    if (pin > 7) {
+        return read(pin - 8, Port::B);
+    }
+    return read(pin, Port::A);
+}
+bool Mcp23017::read(uint8_t pin, Port port) {
+    if (pin > 7) {
+        return false;
+    }
+
+    switch (port) {
+    case Port::A:
+        return readRegister8(Register::GPIOA) & (1 << pin);
+    case Port::B:
+        return readRegister8(Register::GPIOB) & (1 << pin);
+    }
+    return false;
+}
+
+void Mcp23017::write(uint16_t value) { writeRegister16(Register::GPIOA, value); }
+
+void Mcp23017::write(Port port, uint8_t value) {
+    switch (port) {
+    case Port::A:
+        return writeRegister8(Register::GPIOA, value);
+    case Port::B:
+        return writeRegister8(Register::GPIOB, value);
+    }
+}
+
+void Mcp23017::write(uint8_t pin, bool value) {
+    if (pin > 15) {
+        return;
+    }
+
+    if (pin > 7) {
+        return write(pin - 8, Port::B, value);
+    }
+    return write(pin, Port::A, value);
+}
+
+void Mcp23017::write(uint8_t pin, Port port, bool value) {
+    if (pin > 7) {
+        return;
+    }
+
+    Register target_register = Register::GPIOA;
+
+    switch (port) {
+    case Port::A:
+        target_register = Register::GPIOA;
+        break;
+    case Port::B:
+        target_register = Register::GPIOB;
+        break;
+    }
+
+    uint8_t gpio = readRegister8(target_register);
+
+    if (value) {
+        return writeRegister8(target_register, gpio | (1 << pin));
+    } else {
+        return writeRegister8(target_register, gpio & ~(1 << pin));
+    }
+}
+
+uint8_t Mcp23017::readRegister8(Mcp23017::Register reg) {
+    uint8_t result;
+
+    uint8_t reg_addr = static_cast<uint8_t>(reg);
+    i2c_write_blocking(m_i2c, m_address, &reg_addr, 1, true);
+    i2c_read_blocking(m_i2c, m_address, &result, 1, false);
+
+    return result;
+}
+
+uint16_t Mcp23017::readRegister16(Mcp23017::Register reg) {
+    uint8_t result[2];
+
+    uint8_t reg_addr = static_cast<uint8_t>(reg);
+    i2c_write_blocking(m_i2c, m_address, &reg_addr, 1, true);
+    i2c_read_blocking(m_i2c, m_address, result, 2, false);
+
+    return static_cast<uint16_t>(result[1]) << 8 | static_cast<uint16_t>(result[0]);
+}
+
+void Mcp23017::writeRegister8(Mcp23017::Register reg, uint8_t value) {
+    uint8_t reg_addr = static_cast<uint8_t>(reg);
+    uint8_t data[] = {reg_addr, value};
+
+    i2c_write_blocking(m_i2c, m_address, data, 2, false);
+}
+
+void Mcp23017::writeRegister16(Mcp23017::Register reg, uint16_t value) {
+    uint8_t reg_addr = static_cast<uint8_t>(reg);
+    uint8_t data[] = {reg_addr, static_cast<uint8_t>(value & 0x00FF), static_cast<uint8_t>(value >> 8)};
+
+    i2c_write_blocking(m_i2c, m_address, data, 3, false);
+}
